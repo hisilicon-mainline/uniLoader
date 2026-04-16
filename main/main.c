d3acc7c68f5a8828ae74fb71255a96f05ed84eb5
Giovanni Ricca <giovanniricca@proton.me>
2026-04-16 22:31:21 +0200

    bree :3
    Change-Id: Ic71824f7ffe27835e91a334e412544ba1ce8cfe4

--- main/main.c
+++ main/main.c
@@ -24,12 +24,54 @@ static void print_splash(void)
 	printk(KERN_INFO, "welcome to uniLoader %s on %s\n", VER_TAG, board_ops.name);
 }
 
+static void print_umbreon(void)
+{
+	printk(KERN_INFO, "\n");
+	printk(KERN_INFO, "bree :3\n");
+	printk(KERN_INFO, "\n");
+	printk(KERN_INFO, "\n");
+	printk(KERN_INFO, "\n");
+	printk(KERN_INFO, "000000\n");
+	printk(KERN_INFO, "000000\n");
+	printk(KERN_INFO, "000 98000                     000000000\n");
+	printk(KERN_INFO, "000      00              00000      007          000000000\n");
+	printk(KERN_INFO, "000   882  000        000     8     007        00      000\n");
+	printk(KERN_INFO, "000   8    000        000     882   000        00885   000\n");
+	printk(KERN_INFO, "000   2    00000000000   288     000        000     88 000\n");
+	printk(KERN_INFO, "   000    2        000     8  000        000           000\n");
+	printk(KERN_INFO, "   000    5      8 000     6  000        000           000\n");
+	printk(KERN_INFO, "      000 5888   5          00           00018888   000\n");
+	printk(KERN_INFO, "   000    5   885     000000        00000        000\n");
+	printk(KERN_INFO, "   000 8888   5  71   000000        00000        000\n");
+	printk(KERN_INFO, "   000     888809   00   00000000000        00000\n");
+	printk(KERN_INFO, "   000 8885      66600   00000           000\n");
+	printk(KERN_INFO, "   000       7   66600   00000           800\n");
+	printk(KERN_INFO, "   000       7666007     000             5  000\n");
+	printk(KERN_INFO, "      000      7   000000                881000\n");
+	printk(KERN_INFO, "         0000000000                 00028   1  00\n");
+	printk(KERN_INFO, "         0000000000                 000 2   2  00\n");
+	printk(KERN_INFO, "              000                   000  000   00\n");
+	printk(KERN_INFO, "              000     000     000   00000      00\n");
+	printk(KERN_INFO, "              000     000   5 000   00000      00\n");
+	printk(KERN_INFO, "           000     000000  5  000000     000   00\n");
+	printk(KERN_INFO, "         00      00      000  000        000   00\n");
+	printk(KERN_INFO, "         00      00      000  000        000   00\n");
+	printk(KERN_INFO, "         00000000        000  000        000000\n");
+	printk(KERN_INFO, "                         000  000\n");
+	printk(KERN_INFO, "                         000 4\n");
+	printk(KERN_INFO, "                         00000\n");
+	printk(KERN_INFO, "\n");
+	printk(KERN_INFO, "\n");
+	printk(KERN_INFO, "\n");
+}
+
 void main(void* dt, void* kernel, void* ramdisk, void *bootloader_fdt)
 {
 	INITCALL(board_ops.ops.early_init);
 	INITCALL(board_ops.ops.drivers_init);
 
 	print_splash();
+	print_umbreon();
 
 	INITCALL(board_ops.ops.late_init);
 
